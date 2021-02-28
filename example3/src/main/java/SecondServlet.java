ddd6c317a2ff39cfb78bd2892aa4441a9da9b156
Alisher <65455813+alishhh@users.noreply.github.com>
2021-03-01 01:42:01 +0600

    Add files via upload

---
+++ example3/src/main/java/SecondServlet.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class SecondServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+        try{
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String n=request.getParameter("uname");
+        out.print("Hello "+n);
+
+        out.close();
+
+    }catch(Exception e){System.out.println(e);}
+}
+      
+  
+}  
